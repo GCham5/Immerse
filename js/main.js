e415f7033675ffc78e429661a00c25e87c26c87a
GCham5 <georgeschamoun3@hotmail.com>
2023-03-11 15:12:24 -0500

    add dark mode switch

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@ import { playPause, playNext, playPrevious, changeVisual } from "./controls";
 export const audioFiles = ['./music/Havana.m4a', './music/Benzo.m4a', './music/Romantic.m4a', './music/Praise.m4a', './music/LastTime.m4a', './music/DontYouKnow.m4a'];
 
 export let audio = new Audio(audioFiles[0]);
+let isDark = true;
 
+const body = document.body;
+const controls =  document.getElementById('controls');
 const playPauseButton = document.getElementById("play-pause");
 const nextButton = document.getElementById('next');
 const previousButton = document.getElementById('previous');
 const seekBar = document.getElementById('seek'); 
-const dropDown = document.getElementById('dropdown');
+const dropDown = document.getElementById('dropdown'); 
 const playPauseIcon = document.getElementById('play-pause-icon');  
+const lightToggleIcon = document.getElementById('light-toggle');
 
 
 playPauseButton.addEventListener("click", () => playPause(playPauseIcon));
@@ -20,16 +24,32 @@ document.addEventListener("DOMContentLoaded", function() {
 }); 
 
  
-seekBar.addEventListener('change', function() {
+seekBar.addEventListener('change', () => {
   const seekTime = audio.duration * (seekBar.value / 100);
   audio.currentTime = seekTime;  
 });
 
-audio.addEventListener('timeupdate', function() {
+audio.addEventListener('timeupdate', () =>  {
   const currentTime = audio.currentTime;
   const duration = audio.duration;
   const progress = (currentTime / duration) * 100;
   seekBar.value = progress;
   seekBar.style.background = `linear-gradient(to right, black 0%, black ${progress}%, gray ${progress}%, gray 100%)`;
+});
+
+lightToggleIcon.addEventListener('click', () => {
+  if (isDark) {
+    body.style.backgroundColor = 'white';
+    controls.style.backgroundColor = 'gray';
+    lightToggleIcon.classList.remove('fa-sun');
+    lightToggleIcon.classList.add('fa-moon');
+    isDark = false;
+  } else {
+    body.style.backgroundColor = 'black';
+    controls.style.backgroundColor = '#FFFFF0';
+    lightToggleIcon.classList.remove('fa-moon');
+    lightToggleIcon.classList.add('fa-sun');
+    isDark = true;
+  }
 });
 
