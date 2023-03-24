670e4f50ac7c11fbaea6c4fb4e50842af7173a8f
GCham5 <georgeschamoun3@hotmail.com>
2023-03-24 00:08:55 -0400

    fix build

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/Immerse/'
+   base: '/Immerse/',
+   build: {
+    target: "esnext"
+  }
 })
