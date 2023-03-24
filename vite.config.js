170e62f652cac523f7954202f2f71a9cf3e79c2d
GCham5 <georgeschamoun3@hotmail.com>
2023-03-24 00:44:05 -0400

    try fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
    base: '/Immerse/',
    build: {
     target: "esnext"
-  }
+  }, 
+  plugins: [
+    copy({
+      targets: [
+        { src: 'public/music/**/*', dest: 'dist/music' }
+      ]
+    })
+  ]
 })
